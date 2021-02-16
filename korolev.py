c02acff406180ef9a522caf4bd0e9fbf1e00e051
korolevevev <korolev44rus@icloud.com>
2021-02-16 10:56:00 +0300

    Added h1 tag

--- korolev.py
+++ korolev.py
@@ -1,1 +1,4 @@
 print("Hello, World! This is Ilya Korolev's new python program which prints 'Hello, World! This is Ilya Korolev's new python program which prints...'")
+
+for i in range(100):
+    print(100-i, "left")
